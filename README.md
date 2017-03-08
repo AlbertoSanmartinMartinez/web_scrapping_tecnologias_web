2afa27e1f9c8f5b5ceb4303a7b1e7b196f0bac6e
Alberto Sanmartin Martinez <albertosanmartinmartinez@MacBook-Pro-de-Alberto.local>
2017-03-08 09:05:16 +0100

    practica definitva

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # web_scrapping_tecnologias_web
 # codigo que a partir de una url busca el titulo del libro gratuito
-#403 - Forbidden: La solicitud fue legal, pero el servidor rehúsa responderla dado que el cliente no tiene los privilegios para hacerla. He desestimado realizar esta comprobacion porque podia realizar la práctica y extraer el titulo del libro aunque el codigo de la web no fuera 200.
+# 403 - Forbidden: La solicitud fue legal, pero el servidor rehúsa responderla dado que el cliente no tiene los privilegios para hacerla. He desestimado realizar esta comprobacion porque podia realizar la práctica y extraer el titulo del libro aunque el codigo de la web no fuera 200.
+# https://github.com/AlbertoSanmartinMartinez/web_scrapping_tecnologias_web 
--- file.py
+++ file.py
@@ -1,5 +1,5 @@
 
-#!/usr/bin/env python
+# !/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim: set fileencoding=utf8:
 
@@ -11,28 +11,31 @@ Aplicacion para obtener el libro gratuito dirario
 import urllib2
 import requests
 from bs4 import BeautifulSoup
+import os
+
 
 class Book():
     '''clase principal  para el libro'''
 
     def __init__(self, url):
         '''constructor de la clase cliente'''
-        #creamos las variables necesarias
+        # creamos las variables necesarias
         self.url = url
         self.web_code = self.getCode(url)
         self.html_web = self.getWeb(url, self.web_code)
         self.title = self.getTitle(self.html_web)
 
-    def getCode(self,url):
-        '''metodo que obtiene el codigo de la web, no lo usaremos, esta explicado en el reame.md'''
+    def getCode(self, url):
+        '''metodo que obtiene el codigo de la web, explicado en el reame.md'''
+        # obtenemos el codigo que devuelve la web(explicado en el README.md
         req = requests.get(url)
-        code = req.status_code #obtenemos el codigo que devuelve la web(explicado en el README.md
+        code = req.status_code
         return code
 
     def getWeb(self, url, code):
         '''metodo que descarga la web'''
         f = urllib2.urlopen(url)
-        html = f.read() #obtenemos el html de la web
+        html = f.read()  # obtenemos el html de la web
         f.close()
         return html
 
@@ -41,15 +44,25 @@ class Book():
         print "la url es: " + self.url
         print "el codigo de la web es: ", self.web_code
         print "el titulo del libro es: " + self.title.title()
+        self.notify('Notification', 'subtitle', self.title.title())
 
     def getTitle(self, html_web):
         '''metodo que devuelve el titulo dle libro'''
+        # buscamos las etiquetas html donde esta el titulo
         soup = BeautifulSoup(html_web, 'html.parser')
-        elements = soup.find_all('div', 'dotd-title') #buscamos las etiquetas html donde esta el titulo
+        elements = soup.find_all('div', 'dotd-title')
+        # extraemos el texto que contiene el titulo
         for element in elements:
-            result = element.find('h2').text #extraemos el texto que contiene el titulo
+            result = element.find('h2').text
         result = result.strip()
         return result
+
+    def notify(self, title, subtitle, message):
+        t = '-title {!r}'.format(title)
+        s = '-subtitle {!r}'.format(subtitle)
+        m = '-message {!r}'.format(message)
+        os.system('terminal-notifier {}'.format(' '.join([m, t, s])))
+
 
 if __name__ == "__main__":
     '''programa principal'''
